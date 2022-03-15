b44b075d81a30e4c8df75afb3bc0a4a5eb09d79b
stevezyyang <stevezyyang@tencent.com>
2022-03-15 13:27:15 +0800

    refactor: remove useless dirs

--- .vuepress/theme/enhanceApp.js
+++ .vuepress/theme/enhanceApp.js
@@ -1,5 +1,20 @@
-import routes from 'imRouter/routes'
-import importElement from 'imUntil/importElement'
+import {
+  Tag,
+  Row,
+  Col,
+  Menu,
+  Card,
+  Main,
+  Aside,
+  Button,
+  Header,
+  Footer,
+  MenuItem,
+  Container,
+  Pagination,
+  Autocomplete,
+} from 'element-ui'
+import routes from './routes'
 
 import 'imStyles/palette.styl'
 import 'imStyles/index.styl'
@@ -9,6 +24,21 @@ import 'imStyles/code.styl'
 import 'imStyles/content.styl'
 
 export default ({ Vue, router }) => {
+  Vue.use(Tag)
+  Vue.use(Row)
+  Vue.use(Col)
+  Vue.use(Menu)
+  Vue.use(Card)
+  Vue.use(Main)
+  Vue.use(Aside)
+  Vue.use(Button)
+  Vue.use(Header)
+  Vue.use(Footer)
+  Vue.use(MenuItem)
+  Vue.use(Container)
+  Vue.use(Pagination)
+  Vue.use(Autocomplete)
+
   Vue.use(routes, { router })
   Vue.use(importElement)
 }
